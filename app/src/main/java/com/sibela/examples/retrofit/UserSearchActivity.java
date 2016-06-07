b3150aa290d245616f6ef43257ff09d83672bd1d
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-06-07 13:46:17 -0300

    Adding UserSearch.View

--- app/src/main/java/com/sibela/examples/retrofit/UserSearchActivity.java
+++ app/src/main/java/com/sibela/examples/retrofit/UserSearchActivity.java
@@ -3,11 +3,41 @@ package com.sibela.examples.retrofit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class UserSearchActivity extends AppCompatActivity {
+import com.sibela.examples.retrofit.model.User;
+import com.sibela.examples.retrofit.task.UserSearch;
+
+import java.util.List;
+
+public class UserSearchActivity extends AppCompatActivity implements UserSearch.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_search);
+    }
+
+    @Override
+    public void displayUsers(List<User> users) {
+
+    }
+
+    @Override
+    public void displayError() {
+
+    }
+
+    @Override
+    public void showProgress() {
+
+    }
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+    @Override
+    public void searchUsers() {
+
     }
 }

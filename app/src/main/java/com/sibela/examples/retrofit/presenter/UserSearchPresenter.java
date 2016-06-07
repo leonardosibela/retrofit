758f49071b7d5162fea533d785e1e0dd081920fa
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-06-07 14:14:09 -0300

    Implementing UserSearchPresenter

--- app/src/main/java/com/sibela/examples/retrofit/presenter/UserSearchPresenter.java
+++ app/src/main/java/com/sibela/examples/retrofit/presenter/UserSearchPresenter.java
@@ -1,11 +1,44 @@
 package com.sibela.examples.retrofit.presenter;
 
+import android.support.annotation.NonNull;
+
+import com.sibela.examples.retrofit.api.GithubAPI;
+import com.sibela.examples.retrofit.model.UserSearchReturn;
 import com.sibela.examples.retrofit.task.UserSearch;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class UserSearchPresenter implements UserSearch.Presenter {
 
-    @Override
-    public void searchUsers(String name) {
+    private UserSearch.View userSearchView;
 
+    public UserSearchPresenter(@NonNull UserSearch.View userSearchView) {
+        checkNotNull(userSearchView, "UserSearchView cannot be null");
+        this.userSearchView = userSearchView;
+    }
+
+    @Override
+    public void searchUsers(String login) {
+
+        String userSearchByLogin = GithubAPI.Util.toUserSearchByLogin(login);
+
+        Call<UserSearchReturn> usersCall = GithubAPI.getInstance().getUsers(userSearchByLogin);
+        usersCall.enqueue(new Callback<UserSearchReturn>() {
+
+            @Override
+            public void onResponse(Call<UserSearchReturn> call, Response<UserSearchReturn> response) {
+                UserSearchReturn userSearchReturn = response.body();
+                userSearchView.displayUsers(userSearchReturn.getItems());
+            }
+
+            @Override
+            public void onFailure(Call<UserSearchReturn> call, Throwable t) {
+                userSearchView.displayError();
+            }
+        });
     }
 }

7470b5a0faaae1bdfd2c632ba1f5b45ecea5ada8
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-06-07 13:32:08 -0300

    Project skeleton

---
+++ app/src/main/java/com/sibela/examples/retrofit/UserSearchActivity.java
@@ -0,0 +1,13 @@
+package com.sibela.examples.retrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_search);
+    }
+}

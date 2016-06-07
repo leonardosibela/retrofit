a35ba50a6ea9443048c0a472691549b8900bc00f
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-06-07 13:48:27 -0300

    Adding UserSearchPresenter

---
+++ app/src/main/java/com/sibela/examples/retrofit/presenter/UserSearchPresenter.java
@@ -0,0 +1,11 @@
+package com.sibela.examples.retrofit.presenter;
+
+import com.sibela.examples.retrofit.task.UserSearch;
+
+public class UserSearchPresenter implements UserSearch.Presenter {
+
+    @Override
+    public void searchUsers(String name) {
+
+    }
+}

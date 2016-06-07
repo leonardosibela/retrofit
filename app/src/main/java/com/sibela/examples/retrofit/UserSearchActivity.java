63cc3b88ce8793a5dd56cf53c9dd6b451faf6ff1
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-06-07 13:51:17 -0300

    Mapping views and click listeners

--- app/src/main/java/com/sibela/examples/retrofit/UserSearchActivity.java
+++ app/src/main/java/com/sibela/examples/retrofit/UserSearchActivity.java
@@ -1,19 +1,33 @@
 package com.sibela.examples.retrofit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
 
 import com.sibela.examples.retrofit.model.User;
 import com.sibela.examples.retrofit.task.UserSearch;
 
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class UserSearchActivity extends AppCompatActivity implements UserSearch.View {
+
+    @Bind(R.id.users_recycler)
+    RecyclerView userRecycler;
+
+    @Bind(R.id.user_name_input)
+    EditText userNameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_search);
+
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -37,6 +51,7 @@ public class UserSearchActivity extends AppCompatActivity implements UserSearch.
     }
 
     @Override
+    @OnClick(R.id.search_button)
     public void searchUsers() {
 
     }

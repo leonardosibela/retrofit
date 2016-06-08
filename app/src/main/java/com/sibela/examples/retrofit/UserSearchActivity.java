66b677249e4a3c07074f8d95e827c108140e41e2
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-06-08 12:53:36 -0300

    Displaying and hidding progressbar on user search

--- app/src/main/java/com/sibela/examples/retrofit/UserSearchActivity.java
+++ app/src/main/java/com/sibela/examples/retrofit/UserSearchActivity.java
@@ -1,10 +1,14 @@
 package com.sibela.examples.retrofit;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.sibela.examples.retrofit.adapter.StringAdapter;
@@ -26,6 +30,9 @@ public class UserSearchActivity extends AppCompatActivity implements UserSearch.
 
     @Bind(R.id.user_name_input)
     EditText userNameInput;
+
+    @Bind(R.id.user_search_progress)
+    ProgressBar progressBar;
 
     private UserSearch.Presenter presenter;
     private StringAdapter adapter;
@@ -51,27 +58,42 @@ public class UserSearchActivity extends AppCompatActivity implements UserSearch.
     @Override
     public void displayUsers(List<User> users) {
         adapter.setItems(users);
+        hideProgress();
     }
 
     @Override
     public void displayError() {
+        progressBar.setVisibility(View.GONE);
         Toast.makeText(this, R.string.error_searching_users, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void showProgress() {
-
+        progressBar.setVisibility(View.VISIBLE);
+        userRecycler.setVisibility(View.GONE);
     }
 
     @Override
     public void hideProgress() {
-
+        progressBar.setVisibility(View.GONE);
+        userRecycler.setVisibility(View.VISIBLE);
     }
 
     @Override
     @OnClick(R.id.search_button)
     public void searchUsers() {
+        hideKeyboard();
+        showProgress();
         String login = userNameInput.getText().toString();
         presenter.searchUsers(login);
+    }
+
+    public void hideKeyboard() {
+        View view = getCurrentFocus();
+        getImputMethodManager().hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    private InputMethodManager getImputMethodManager() {
+        return (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
     }
 }

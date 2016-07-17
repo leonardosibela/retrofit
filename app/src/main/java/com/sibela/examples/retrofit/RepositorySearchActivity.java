3fb5cdc33246de92b241fba0bd751bdd038b1708
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-07-17 00:08:02 -0300

    Adding repository search activity

---
+++ app/src/main/java/com/sibela/examples/retrofit/RepositorySearchActivity.java
@@ -0,0 +1,13 @@
+package com.sibela.examples.retrofit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RepositorySearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository_search);
+    }
+}

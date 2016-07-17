51d55a048ae2f9f0ea1c337b5f3dd4db4c7e0b6b
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-07-17 00:15:34 -0300

    Adding getters and setters for repository

--- app/src/main/java/com/sibela/examples/retrofit/model/Repository.java
+++ app/src/main/java/com/sibela/examples/retrofit/model/Repository.java
@@ -8,4 +8,35 @@ public class Repository {
     private String description;
 
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
